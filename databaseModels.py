75fcb82705bb58cb85cd8656a4ceec1f89700e4c
michi <mgemmel@students.uni-mainz.de>
2026-05-26 23:46:50 +0200

    Pub quiz temp host

--- databaseModels.py
+++ databaseModels.py
@@ -160,3 +160,32 @@ class UptimeTracker(db.Model):
     function = db.Column(db.String(50))
     time = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     duration = db.Column(db.String(50))
+    
+    
+    
+class PubQuizRounds(db.Model):
+    __tablename__ = 'pubquiz_rounds'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    datum = db.Column(db.DateTime)
+    bilderrunde = db.Column(db.String(100))
+    quizmaster = db.Column(db.String(100))
+
+    teams = db.relationship('PubQuizTeams', back_populates='round', lazy=True, cascade="all, delete-orphan")
+
+class PubQuizTeams(db.Model):
+    __tablename__ = 'pubquiz_teams'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100))
+    round_id = db.Column(db.Integer, db.ForeignKey('pubquiz_rounds.id'))
+    round1_points = db.Column(db.Integer)
+    round2_points = db.Column(db.Integer)
+    round3_points = db.Column(db.Integer)
+    round4_points = db.Column(db.Integer)
+    round1_size = db.Column(db.Integer)
+    round2_size = db.Column(db.Integer)
+    round3_size = db.Column(db.Integer)
+    round4_size = db.Column(db.Integer)
+
+    round = db.relationship('PubQuizRounds', back_populates='teams')
+    
+    
